29fd85360eb8c8f6a1ee100f3b8054540b4691d4
medina003 <aliyeva.medina03@gmail.com>
2024-12-15 14:08:54 +0400

    teamUpdated2

--- Data/Dtos/Staffs/StaffSummaryDto.cs
+++ Data/Dtos/Staffs/StaffSummaryDto.cs
@@ -2,6 +2,7 @@
 {
     public class StaffSummaryDto
     {
+        public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Position { get; set; }
